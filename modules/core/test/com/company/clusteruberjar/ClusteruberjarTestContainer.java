f24a173cb85562e151a6d10d5c0a3c67f859a35e
Sergei Koldaev <s.koldaev@haulmont.com>
2021-03-18 19:39:15 +0300

    initial commit

---
+++ modules/core/test/com/company/clusteruberjar/ClusteruberjarTestContainer.java
@@ -0,0 +1,57 @@
+package com.company.clusteruberjar;
+
+import com.haulmont.cuba.testsupport.TestContainer;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ClusteruberjarTestContainer extends TestContainer {
+
+    public ClusteruberjarTestContainer() {
+        super();
+        //noinspection ArraysAsListWithZeroOrOneArgument
+        appComponents = Arrays.asList(
+                "com.haulmont.cuba",
+                "com.haulmont.addon.cubajm");
+        appPropertiesFiles = Arrays.asList(
+                // List the files defined in your web.xml
+                // in appPropertiesConfig context parameter of the core module
+                "com/company/clusteruberjar/app.properties",
+                // Add this file which is located in CUBA and defines some properties
+                // specifically for test environment. You can replace it with your own
+                // or add another one in the end.
+                "com/company/clusteruberjar/test-app.properties");
+        autoConfigureDataSource();
+    }
+
+    
+
+    public static class Common extends ClusteruberjarTestContainer {
+
+        public static final ClusteruberjarTestContainer.Common INSTANCE = new ClusteruberjarTestContainer.Common();
+
+        private static volatile boolean initialized;
+
+        private Common() {
+        }
+
+        @Override
+        public void beforeAll(ExtensionContext extensionContext) throws Exception {
+            if (!initialized) {
+                super.beforeAll(extensionContext);
+                initialized = true;
+            }
+            setupContext();
+        }
+        
+
+        @SuppressWarnings("RedundantThrows")
+        @Override
+        public void afterAll(ExtensionContext extensionContext) throws Exception {
+            cleanupContext();
+            // never stops - do not call super
+        }
+        
+    }
+}
